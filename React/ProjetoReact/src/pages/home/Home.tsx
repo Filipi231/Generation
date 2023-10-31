5bbae85f2d1c1a0afa873e61b6cce34fd8b3d12f
unknown <Filipi231>
2023-10-31 10:34:21 -0300

    react

---
+++ React/ProjetoReact/src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import './Home.css'
+import homeLogo from '../../assets/login-img.jpg'
+
+function Home() {
+  return (
+    <>
+            <h1 className='titulo'>Home</h1>
+
+            <img src={homeLogo} className='img' alt="Imagem da Tela Inicial" />
+    </>
+
+  )
+}
+
+export default Home 
