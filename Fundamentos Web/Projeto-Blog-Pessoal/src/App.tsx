be73e54b46a7becbde2c401d68fb549c748e2e86
unknown <Filipi231>
2023-11-01 10:13:56 -0300

    alteracao

--- Fundamentos Web/Projeto-Blog-Pessoal/src/App.tsx
+++ Fundamentos Web/Projeto-Blog-Pessoal/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
    <Home/>
-   <Card titulo='Anime' descricao='Dragon Ball' />
+   <Card titulo='Blog' descricao='Curtidas' />
    <Contador />
    <Tarefa/>
    </div>
