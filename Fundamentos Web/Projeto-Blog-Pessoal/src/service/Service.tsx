f5493badfe55fe60c21a3752a48c29239a1e1f82
Filipi <filipibarbosa750@outlook.com>
2023-11-09 15:35:36 -0300

    adicao de temas

--- Fundamentos Web/Projeto-Blog-Pessoal/src/service/Service.tsx
+++ Fundamentos Web/Projeto-Blog-Pessoal/src/service/Service.tsx
@@ -6,12 +6,31 @@ const api = axios.create({
     baseURL: 'https://blogpesoal.onrender.com'
 })
 
-export const login = async(url: string, Dados: Object, setDados: Function) => {
-    const resposta = await api.post(url, Dados)
-    setDados(resposta.data)
-}
-
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
+  }
+  
+  export const login = async(url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+  }
+  
+  export const buscar = async(url: string, setDados: Function, header: Object) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+  }
+  
+  export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+  }
+  
+  export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+  }
+  
+  export const deletar = async(url: string, header: Object) => {
+    await api.delete(url, header)
   }
